8b9dea92cc923e2f09eea44ebe0bde56b3273c29
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-23 01:03:12 +1100

    Swap new SimpleParticleType to FabricParticleType#simple.

--- fabric/src/main/java/vectorwing/farmersdelight/common/registry/ModParticleTypes.java
+++ fabric/src/main/java/vectorwing/farmersdelight/common/registry/ModParticleTypes.java
@@ -2,6 +2,7 @@ package vectorwing.farmersdelight.common.registry;
 
 import io.github.fabricators_of_create.porting_lib.util.LazyRegistrar;
 import io.github.fabricators_of_create.porting_lib.util.RegistryObject;
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -12,7 +13,7 @@ public class ModParticleTypes
 	public static final LazyRegistrar<ParticleType<?>> PARTICLE_TYPES = LazyRegistrar.create(BuiltInRegistries.PARTICLE_TYPE, FarmersDelight.MODID);
 
 	public static final RegistryObject<SimpleParticleType> STAR = PARTICLE_TYPES.register("star",
-			() -> new SimpleParticleType(true));
+			() -> FabricParticleTypes.simple(true));
 	public static final RegistryObject<SimpleParticleType> STEAM = PARTICLE_TYPES.register("steam",
-			() -> new SimpleParticleType(true));
+			() -> FabricParticleTypes.simple(true));
 }

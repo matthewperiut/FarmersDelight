c316757ed0125227c2fb51a104f3d927356fbd72
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-23 00:14:04 +1100

    Use ExtendedScreenHandlerType instead of IForgeMenuType.create.

--- fabric/src/main/java/vectorwing/farmersdelight/common/registry/ModMenuTypes.java
+++ fabric/src/main/java/vectorwing/farmersdelight/common/registry/ModMenuTypes.java
@@ -2,6 +2,7 @@ package vectorwing.farmersdelight.common.registry;
 
 import io.github.fabricators_of_create.porting_lib.util.LazyRegistrar;
 import io.github.fabricators_of_create.porting_lib.util.RegistryObject;
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.inventory.MenuType;
 import vectorwing.farmersdelight.FarmersDelight;
@@ -12,5 +13,5 @@ public class ModMenuTypes
 	public static final LazyRegistrar<MenuType<?>> MENU_TYPES = LazyRegistrar.create(BuiltInRegistries.MENU, FarmersDelight.MODID);
 
 	public static final RegistryObject<MenuType<CookingPotMenu>> COOKING_POT = MENU_TYPES
-			.register("cooking_pot", () -> IForgeMenuType.create(CookingPotMenu::new));
+			.register("cooking_pot", () -> new ExtendedScreenHandlerType<>(CookingPotMenu::new));
 }

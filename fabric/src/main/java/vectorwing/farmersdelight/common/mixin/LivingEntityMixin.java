d757a633ca213ef90f3789d27b5cd8a4851d5774
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-24 17:31:34 +1100

    Only allow tomatoes to be climbable whilst ropelogged.

--- fabric/src/main/java/vectorwing/farmersdelight/common/mixin/LivingEntityMixin.java
+++ fabric/src/main/java/vectorwing/farmersdelight/common/mixin/LivingEntityMixin.java
@@ -1,15 +1,27 @@
 package vectorwing.farmersdelight.common.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import vectorwing.farmersdelight.common.block.TomatoVineBlock;
 import vectorwing.farmersdelight.common.item.SkilletItem;
 import vectorwing.farmersdelight.common.item.enchantment.BackstabbingEnchantment;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @ModifyVariable(method = "actuallyHurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getDamageAfterArmorAbsorb(Lnet/minecraft/world/damagesource/DamageSource;F)F"), argsOnly = true)
     private float handleBackstabbingDamage(float original, DamageSource damageSource) {
         if (original > 0) {
@@ -18,5 +30,10 @@ public class LivingEntityMixin {
             return BackstabbingEnchantment.BackstabbingEvent.onKnifeBackstab((LivingEntity)(Object)this, damageSource, original);
         }
         return original;
+    }
+
+    @ModifyExpressionValue(method = "onClimbable", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/tags/TagKey;)Z"))
+    private boolean onlyAllowTomatoClimbingWhilstRopelogged(boolean original, @Local BlockPos pos, @Local BlockState state) {
+        return original && (!(state.getBlock() instanceof TomatoVineBlock tomato) || tomato.isLadder(state, this.level(), pos, (LivingEntity)(Object)this));
     }
 }
